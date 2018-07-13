236cc799d33f67d8e362480dc7bee83636bf308f
Kishor Maharjan <lasataya@gmail.com>
2018-07-13 14:22:13 +1000

    overlap animation

--- script.js
+++ script.js
@@ -1,27 +1,23 @@
 (function($){
-  $.fn.encode = function(options){
-    var settings = $.extend({}, $.fn.encode.defaults, options);
+  $.fn.textFieldEffects = function(options){
+    
+    var defaultOptions = {
+      animationType : 'overlap'
+    }
+    
+    options = $.extend({}, defaultOptions, options);
     
     return this.each(function(){
       var $this = $(this);
-      var currentValue = $this.text();
-      var newValue = '', i = 0;
-      
-      for(i = 0; i < currentValue.length; i++){
-        if(settings.action === 'scramble'){
-          newValue = newValue + String.fromCharCode(currentValue.charCodeAt(i) + 1);
-        }
-        else if(settings.action === 'unscramble'){
-          newValue = newValue + String.fromCharCode(currentValue.charCodeAt(i) - 1);          
-        }
+      if(defaultOptions.animationType === 'overlap'){
+        overlap($this);
       }
-      return $this.text(newValue).css('color', settings.action === 'scramble' ? settings.scrambleColor : settings.unscrambleColor );
-      
     });
-  }
-  $.fn.encode.defaults = {
-    action: 'scramble',
-    scrambleColor: 'blue',
-    unscrambleColor: 'black'
+    
+    function overlap($this){
+      $this.on('focus blur', 'input', function(e){
+        $this.closest('.js-form-group--overlap').toggleClass('isFocused', e.type === 'focusin' || this.value.length > 0);
+      })
+    }
   }
 })(jQuery);
