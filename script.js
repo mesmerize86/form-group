98a261fed70ea36cd3aabf3574071967bf8027d2
Kishor Maharjan <lasataya@gmail.com>
2018-07-13 16:12:43 +1000

    background animation added

--- script.js
+++ script.js
@@ -1,23 +1,10 @@
 (function($){
-  $.fn.textFieldEffects = function(options){
-    
-    var defaultOptions = {
-      animationType : 'overlap'
-    }
-    
-    options = $.extend({}, defaultOptions, options);
-    
+  $.fn.textFieldEffects = function(){
     return this.each(function(){
       var $this = $(this);
-      if(defaultOptions.animationType === 'overlap'){
-        overlap($this);
-      }
-    });
-    
-    function overlap($this){
       $this.on('focus blur', 'input', function(e){
-        $this.closest('.js-form-group--overlap').toggleClass('isFocused', e.type === 'focusin' || this.value.length > 0);
+        $this.closest('div').toggleClass('isFocused', e.type === 'focusin' || this.value.length > 0);
       })
-    }
+    });    
   }
 })(jQuery);
